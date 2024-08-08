4e771372f9ec904a25a1353f67bdb7d3b92e6cb4
Devin-Diaz <diazdevin7@outlook.com>
2024-08-08 19:25:24 -0400

    adding files

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Root from './Components/Root';
+
+
+function App() {
+  return (
+    <Root />
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../Styles/navbarStyle.css'
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <a className='title' href='/'>devin-diaz@portfolio:</a>
+            <ul>
+                <li>
+                    <a href='/'>~/</a>
+                </li>
+
+                <li>
+                    <a href='/projects'>/projects</a>
+                </li>
+
+                <li>
+                    <a href='/resume'>/resume</a>
+                </li>
+
+                <li>
+                    <a href='/'>/contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
